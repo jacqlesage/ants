fe7c727d674f1ffcaaa6ac2706b0df4284ba09d2
jacqlesage <jacqlesages@gmail.com>
2016-03-01 22:26:49 +1300

    added comments to our code

--- src/antsEtude1/JavaApplication1.java
+++ src/antsEtude1/JavaApplication1.java
@@ -26,7 +26,12 @@ public class JavaApplication1 {
         Integer numSteps = readInput(DNAlist);
         startAntJourney(DNAlist, numSteps);
     }
-
+    /**
+     * A method which reads in input and makes changes on it as per customers requirements
+     * 
+     * @param DNAlist - An Arraylist which holds the lines from the input
+     * @return numSteps - the amount of lines to process
+     */
     private static Integer readInput(ArrayList<DNA> DNAlist) {
         Integer numSteps = 0;
         String s;
@@ -39,82 +44,84 @@ public class JavaApplication1 {
         }
         Scanner in = new Scanner(System.in);
 
-        //test line
-        //System.out.println("Enter a string");
         //get the line coming in
         while (in.hasNextLine()) {
             s = in.nextLine();
             //check if this line is a # - this should be comments so ignore
             if (s.charAt(0) == '#') {
-//                System.out.println("in if statement, comment found");
-                //System.out.println(s);
-                // check if character is digit
+                // check if character is digit - then we are at the number of times/lines there will be
             } else if (Character.isDigit(s.charAt(0))) {
                 numSteps = Integer.parseInt(s);
-//                System.out.println("in int statement");
-//                System.out.println(numSteps);
+
             } else {
-                
+             //we have to capture the input from the line and break it down for our DNA constructor   
                 System.out.println(s + "\n");
 //                setup the DNA for the ant
                 char DNA_State = s.charAt(0);
-//                System.out.println("state = " + DNA_State);
 
                 char compassN = s.charAt(2);
                 char compassE = s.charAt(3);
                 char compassS = s.charAt(4);
                 char compassW = s.charAt(5);
 
-//                System.out.println("compass = " + compassN + " " + compassE + " " + compassS + " " + compassW);
-
                 char DNAToStateN = s.charAt(7);
                 char DNAToStateE = s.charAt(8);
                 char DNAToStateS = s.charAt(9);
                 char DNAToStateW = s.charAt(10);
 
-//                System.out.println("DNAToState = " + DNAToStateN + " " + DNAToStateE + " " + DNAToStateS + " " + DNAToStateW);
-
+                //place the info into our constructor for the DNA object
                 DNA e = new DNA(DNA_State, compassN, compassE, compassS, compassW, DNAToStateN, DNAToStateE, DNAToStateS, DNAToStateW);
 
                 DNAlist.add(e);
 
             }
         }
+        /*
+        A test for the objects
         for (DNA e : DNAlist) {
-//        System.out.println(e.toString());
+        //System.out.println(e.toString());
         }
+        */
         return numSteps;
     }
-    
+    /**
+     * A method which in theory moves an ant around keeping track of the "Point"
+     * and checking that if the ant goes back to a previously visited point, what that 
+     * state was, and finally the final position of the Ant
+     * 
+     * @param DNAList the array list of input
+     * @param numSteps - the number of steps to take
+     * 
+     */
     private static void startAntJourney(ArrayList<DNA> DNAlist, Integer numSteps) {
         HashMap grid = new HashMap();
         char currentState = ' ';
         Point currentPoint;
         char initialState = DNAlist.get(0).getState();
-//        System.out.println("initial state = " + initialState);
+
         grid.put(new Point(0,0), initialState);
         Ant ant = new Ant('N', new Point(0,0));
-//        System.out.println(ant.toString());
-//        System.out.println("numSteps = " + numSteps);
+
         for (int i = 1; i < numSteps; i++) {
-//            System.out.println("in for loop");
+
             currentPoint = ant.getLocation();
-//            System.out.println("current point = " + currentPoint);
+
             if (grid.containsKey(currentPoint)) {
                 currentState = (char) grid.get(currentPoint);
             } else {
                 grid.put(currentPoint, initialState);
                 currentState = initialState;
             }
-//            System.out.println("current state = " + currentState);
+
             for (DNA dna : DNAlist) {
                 char dna_state = dna.getState();
                 if (currentState == dna_state) {
                     dna.moveAnt(ant, grid);
                 }
             }
-//            System.out.println(grid.toString());
+
         }
+        //print out of the input as the customer requested.
         Point finalLocation = ant.getLocation();
         int x = finalLocation.x;
         int y = finalLocation.y;
