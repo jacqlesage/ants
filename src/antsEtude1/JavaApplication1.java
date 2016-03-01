0ab7a696553505a0cbf1ed77dd4b9560a688f03a
prabhatgokul <prabhatgokul@gmail.com>
2016-03-01 16:33:37 +1300

    complete startAntjourney method

--- src/antsEtude1/JavaApplication1.java
+++ src/antsEtude1/JavaApplication1.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -17,16 +18,17 @@ import java.util.Scanner;
  * @author James
  */
 public class JavaApplication1 {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ArrayList<DNA> DNAlist = new ArrayList<DNA>();
-        readInput(DNAlist);
+        ArrayList<DNA> DNAlist = new ArrayList<>();
+        Integer numSteps = readInput(DNAlist);
+        startAntJourney(DNAlist, numSteps);
     }
 
-    private static void readInput(ArrayList<DNA> DNAlist) {
+    private static Integer readInput(ArrayList<DNA> DNAlist) {
+        Integer numSteps = 0;
         String s;
         try {
             FileInputStream is = new FileInputStream(new File("C:\\Users\\admin\\Documents\\Otago uni\\COSC326\\ants\\ants\\src\\antsEtude1\\input.txt"));
@@ -44,32 +46,33 @@ public class JavaApplication1 {
             s = in.nextLine();
             //check if this line is a # - this should be comments so ignore
             if (s.charAt(0) == '#') {
-                System.out.println("in if statement, comment found");
-                //s = in.nextLine();
+//                System.out.println("in if statement, comment found");
                 //System.out.println(s);
                 // check if character is digit
             } else if (Character.isDigit(s.charAt(0))) {
-                Integer stepsToTake = Integer.parseInt(s);
-                System.out.println("in int statement");
-                System.out.println(stepsToTake);
+                numSteps = Integer.parseInt(s);
+//                System.out.println("in int statement");
+//                System.out.println(numSteps);
             } else {
-                //setup the DNA for the ant
+                
+                System.out.println(s + "\n");
+//                setup the DNA for the ant
                 char DNA_State = s.charAt(0);
-                System.out.println("state = " + DNA_State);
+//                System.out.println("state = " + DNA_State);
 
                 char compassN = s.charAt(2);
                 char compassE = s.charAt(3);
                 char compassS = s.charAt(4);
                 char compassW = s.charAt(5);
 
-                System.out.println("compass = " + compassN + " " + compassE + " " + compassS + " " + compassW);
+//                System.out.println("compass = " + compassN + " " + compassE + " " + compassS + " " + compassW);
 
                 char DNAToStateN = s.charAt(7);
                 char DNAToStateE = s.charAt(8);
                 char DNAToStateS = s.charAt(9);
                 char DNAToStateW = s.charAt(10);
 
-                System.out.println("DNAToState = " + DNAToStateN + " " + DNAToStateE + " " + DNAToStateS + " " + DNAToStateW);
+//                System.out.println("DNAToState = " + DNAToStateN + " " + DNAToStateE + " " + DNAToStateS + " " + DNAToStateW);
 
                 DNA e = new DNA(DNA_State, compassN, compassE, compassS, compassW, DNAToStateN, DNAToStateE, DNAToStateS, DNAToStateW);
 
@@ -78,7 +81,43 @@ public class JavaApplication1 {
             }
         }
         for (DNA e : DNAlist) {
-        System.out.println(e.toString());
+//        System.out.println(e.toString());
         }
+        return numSteps;
+    }
+    
+    private static void startAntJourney(ArrayList<DNA> DNAlist, Integer numSteps) {
+        HashMap grid = new HashMap();
+        char currentState = ' ';
+        Point currentPoint;
+        char initialState = DNAlist.get(0).getState();
+//        System.out.println("initial state = " + initialState);
+        grid.put(new Point(0,0), initialState);
+        Ant ant = new Ant('N', new Point(0,0));
+//        System.out.println(ant.toString());
+//        System.out.println("numSteps = " + numSteps);
+        for (int i = 1; i < numSteps; i++) {
+//            System.out.println("in for loop");
+            currentPoint = ant.getLocation();
+//            System.out.println("current point = " + currentPoint);
+            if (grid.containsKey(currentPoint)) {
+                currentState = (char) grid.get(currentPoint);
+            } else {
+                grid.put(currentPoint, initialState);
+                currentState = initialState;
+            }
+//            System.out.println("current state = " + currentState);
+            for (DNA dna : DNAlist) {
+                char dna_state = dna.getState();
+                if (currentState == dna_state) {
+                    dna.moveAnt(ant, grid);
+                }
+            }
+//            System.out.println(grid.toString());
+        }
+        Point finalLocation = ant.getLocation();
+        int x = finalLocation.x;
+        int y = finalLocation.y;
+        System.out.println("# " + x + " " + y);
     }
 }
